4d22c4a9750231532c977968668c62813d88d7b7
byio <ben@benyang.io>
2018-03-06 21:05:15 +0800

    create EmployeeCreate component, add right navbar button to employeeList scene to navigate to employeeCreate scene

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -12,7 +13,15 @@ const RouterComponent = () => {
           <Scene key="login" component={LoginForm} title="Please Login" />
         </Scene>
         <Scene key="main">
-          <Scene key="employeeList" component={EmployeeList} title="Your Employees" />
+          <Scene
+            rightTitle="Add"
+            onRight={ () => Actions.employeeCreate() }
+            key="employeeList"
+            component={EmployeeList}
+            title="Your Employees"
+            initial
+          />
+          <Scene key="employeeCreate" component={EmployeeCreate} title="Add New Employee" />
         </Scene>
       </Scene>
     </Router>
