49eb566077aa0ed617258f30d4012bd2eaf26213
byio <ben@benyang.io>
2018-03-16 22:41:33 +0800

    connect employeeUpdate action creator to employee form, and handle state changes in name and phone fields

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, TextField, Button } from './common';
 
 class EmployeeCreate extends Component {
+  // render method
   render () {
     return (
       <Card>
@@ -10,6 +14,12 @@ class EmployeeCreate extends Component {
           <TextField
             label="Name"
             placeholderText="eg. Jane Doe"
+            value={this.props.name}
+            onChangeText={
+              value => this.props.employeeUpdate({
+                prop: 'name',
+                value
+              })}
           />
         </CardSection>
         {/* Employee's Contact Number */}
@@ -17,6 +27,12 @@ class EmployeeCreate extends Component {
           <TextField
             label="Phone"
             placeholderText="555-555-5555"
+            value={this.props.phone}
+            onChangeText={
+              value => this.props.employeeUpdate({
+                prop: 'phone',
+                value
+              })}
           />
         </CardSection>
         <CardSection>
@@ -33,4 +49,13 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm;
+  // return as props
+  return { name, phone, shift };
+};
+
+export default connect(
+  mapStateToProps,
+  { employeeUpdate }
+)(EmployeeCreate);
