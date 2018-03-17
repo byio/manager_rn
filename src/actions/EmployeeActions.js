2c3dc1e48dd37e18bfcb0779abf63ff742471e07
byio <ben@benyang.io>
2018-03-17 17:21:11 +0800

    create action creator to dispatch employee_fetch_success action upon successfully fetching list of employees from firebase db

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS } from './types';
 
 /*
   this employeeUpdate action creator handles ANY update to
@@ -27,6 +27,19 @@ export const employeeCreate = ({ name, phone, shift }) => {
             .then(() => {
               dispatch({ type: EMPLOYEE_CREATE });
               Actions.pop();
+            });
+  };
+};
+
+export const employeesFetch = () => {
+  // extract current user from firebase auth
+  const { currentUser } = firebase.auth();
+  // use redux-thunk while fetching data from firebase (async function)
+  return (dispatch) => {
+    firebase.database()
+            .ref(`/users/${currentUser.uid}/employees`)
+            .on('value', snapshot => {
+              dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
             });
   };
 };
--- src/actions/types.js
+++ src/actions/types.js
@@ -9,3 +9,4 @@ export const LOGOUT_USER = 'logout_user';
 // Employee related action types
 export const EMPLOYEE_UPDATE = 'employee_update';
 export const EMPLOYEE_CREATE = 'employee_create';
+export const EMPLOYEES_FETCH_SUCCESS = 'employees_fetch_success';
