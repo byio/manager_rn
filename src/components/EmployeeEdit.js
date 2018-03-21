d3043ba5b615401ee86e3c2db969c22e21aa6d8e
byio <ben@benyang.io>
2018-03-21 19:54:28 +0800

    display respective employee data on edit form, and wire up save changes button to fire and save onto the reducer and redux store (not wired up to firebase db yet)

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,16 +1,44 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Button } from './common';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
+  // lifecycle methods
+  /*
+    when this component is about to be mounted, take the
+    props (this.props.employee) passed from EmployeeListItem
+    component via RNRF's Actions, and pass them to the
+    employeeUpdate reducer with the help of lodash.
+  */
+  componentWillMount () {
+    /*
+      iterate over every property (key) in employee object,
+      and invoke the employeeUpdate function with these
+      key value pairs.
+    */
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({ prop, value });
+    });
+  }
+
+  // helper methods
+  onButtonPress () {
+    const { name, phone, shift } = this.props;
+    console.log(name, phone, shift);
+  }
+
+  // render method
   render () {
+    // console.log(this.props.employee);
     return (
       <Card>
         <EmployeeForm />
         <CardSection>
-          <Button>
+          <Button onPressProp={this.onButtonPress.bind(this)}>
             Save Changes
           </Button>
         </CardSection>
@@ -19,4 +47,12 @@ class EmployeeEdit extends Component {
   }
 }
 
-export default connect()(EmployeeEdit);
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+}
+
+export default connect(
+  mapStateToProps,
+  { employeeUpdate }
+)(EmployeeEdit);
