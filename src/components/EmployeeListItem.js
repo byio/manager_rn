264ace84783a90c8ad1d112974879fc287e9be27
byio <ben@benyang.io>
2018-03-21 17:25:52 +0800

    navigate to emplyee create form when employee name (employee list item) is tapped

--- src/components/EmployeeListItem.js
+++ src/components/EmployeeListItem.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { TouchableWithoutFeedback, View, Text } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 
 import { CardSection } from './common';
 
 class EmployeeListItem extends Component {
 
+  // helper methods
+  onRowPress () {
+    Actions.employeeCreate();
+  }
+
+  // render method
   render () {
     const { item } = this.props.employeeProp;
 
     return (
-      <CardSection>
-        <Text style={styles.titleStyle}>
-          {item.name}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+        <View>
+          <CardSection>
+            <Text style={styles.titleStyle}>
+              {item.name}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 
